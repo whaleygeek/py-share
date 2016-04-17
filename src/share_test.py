e6d38ca8d8d97b794cef7e9fa1860cd15ea696e5
David Whale <david@thinkingbinaries.com>
2016-04-17 19:53:51 +0100

    Improved usability of tester to work with multiple processes

--- src/share_test.py
+++ src/share_test.py
@@ -3,11 +3,11 @@
 # Test the generic share mechanism
 
 import share
+import time
 
 
 def tx():
     share.face("happy")
-
 
 
 def rx():
@@ -17,14 +17,33 @@ def rx():
 
 
 def test():
-    import time
     for i in range(10):
         tx()
         rx()
         time.sleep(0.5)
 
 
+def test_tx():
+    for i in range(10):
+        tx()
+        time.sleep(0.5)
+
+
+def test_rx():
+    for i in range(10):
+        rx()
+        time.sleep(0.5)
+
+
 if __name__ == "__main__":
-    test()
+    import sys
+    if len(sys.argv) == 1:
+        test()
+    else:
+        mode = sys.argv[1]
+        if mode == "tx":
+            test_tx()
+        elif mode == "rx":
+            test_rx()
 
 # END
