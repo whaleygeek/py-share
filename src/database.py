8c54817977cc629ed666a1b66d7403aacf57b28e
David Whale <david@thinkingbinaries.com>
2016-05-11 18:02:20 +0100

    Added a sample test harness

--- src/database.py
+++ src/database.py
@@ -24,10 +24,13 @@
 #   write
 #     set(key, value)     - set a key to a value
 #     delete(key)         - delete a key
+#     sync([from,to])     - synchronise the database from remote copies
+#     push([from,to])     - republish data between two revisions to all remote copies
 #   read
 #     check(key)->flag    - check if a key exists
 #     get(key)->value     - get the value associated with a key
 #     list()->keys(iter)  - list all keys
+
 
 # Store - a local cache of values, possibly non-volatile, possibly platform-dependent format
 #   lifecycle
@@ -46,6 +49,9 @@
 #     get(key)->value     - get the value associated with a key
 #     list()->keys(iter)  - get all keys
 
+# DBMStore - a dbm implementation of a Store()
+#TODO same interface as Store()
+
 # Protocol - the wire protocol for exchanging updates via some form of transport
 #  lifecycle
 #    start(topic)         - start a communications session
@@ -63,5 +69,41 @@
 #    do_set(key, value)   - set a key to a value
 #    do_delete(key)       - delete a key
 #    do_request(from, to) - request history to be sent between two revisions
+
+
+# Multicast - a network layer that communicates via multicasts on a UDP port
+# lifecycle
+#   start
+#   stop
+# transfer
+#   send
+#   check
+#   peek
+#   read
+#   on_incoming
+
+
+#----- TEST HARNESS -----------------------------------------------------------
+
+# db = Database(Store, Protocol(Multicast))
+# db.create("secrets", "0.0.0.0:8080")
+
+# db.write("password", "fred")
+# db.write("password", "bert")
+# db["password"] = "harry"
+
+# if db.check("password"):
+#   print(db.read("password")
+# print(db["password"])
+
+# db.delete("password")
+# del db["password"]
+
+# db.sync()
+# db.push()
+# db.push(0,100)
+# db.push(0,db.revision()-1)
+
+
 
 # END
